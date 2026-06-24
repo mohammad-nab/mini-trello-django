8caa97201b9412e7f242c6faca959ecfd1f6e6e6
Mohammad <mohammadnabavi1384@gmail.com>
2026-06-24 18:24:01 +0330

    project-models-migrations

--- conf/settings.py
+++ conf/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
+    'projects.apps.ProjectsConfig',
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mini_trello',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
