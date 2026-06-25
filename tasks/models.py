0e1548bdbd588e01acba2df8ca60f9c912779373
Mohammad <mohammadnabavi1384@gmail.com>
2026-06-25 15:25:15 +0330

    columns & tasks models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from projects.models import Project
 
-# Create your models here.
+
+class Column(models.Model):
+    project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='columns')
+    title = models.CharField(max_length=50)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.project}:{self.title}'
+
+
+class Task(models.Model):
+    column = models.ForeignKey(Column,on_delete=models.CASCADE,related_name='tasks')
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    assigned_to = models.ForeignKey(User,on_delete=models.CASCADE,related_name='assigned_to')
+    order = models.PositiveIntegerField(default=0)
