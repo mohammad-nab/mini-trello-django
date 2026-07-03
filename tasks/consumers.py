e0d101988af880c87bda9e1139df1a657fa0d17f
Mohammad <mohammadnabavi1384@gmail.com>
2026-07-03 13:26:07 +0330

    redis container

--- tasks/consumers.py
+++ tasks/consumers.py
@@ -12,3 +12,7 @@ class ProjectConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
+        print(text_data_json)
+        self.send(text_data=json.dumps({
+            "message": "Hello from Django!"
+        }))
